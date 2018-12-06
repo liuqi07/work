ba3b533e43689948a0941eec5e525e84d58f45c4
liuqi <liuqi@unipus.cn>
2018-12-06 17:32:35 +0800

    删除age

--- src/view/courseAdviser/courseAdviserList.vue
+++ src/view/courseAdviser/courseAdviserList.vue
@@ -44,9 +44,9 @@
             <Radio :label="2">女</Radio>
           </RadioGroup>
         </FormItem>
-        <FormItem prop="age" label="年龄：" style="width: 300px;" >
+        <!-- <FormItem prop="age" label="年龄：" style="width: 300px;" >
           <Input v-model="updateDetailData.age" placeholder="请输入年龄" @on-change="onAgeChange" ></Input>
-        </FormItem>
+        </FormItem> -->
         <FormItem prop="rate" label="提成比例：" style="width: 320px;" >
           <Row><Input v-model="updateDetailData.rate" style="width: 200px;" /> %</Row>
         </FormItem>
@@ -80,7 +80,7 @@
               if(!res.data){
                  cb(new Error('身份证号已存在'))
               }else{
-                this.updateDetailData.age = this._age ? this._age : (new Date().getFullYear() - idNo.slice(6, 10)).toString()
+                // this.updateDetailData.age = this._age ? this._age : (new Date().getFullYear() - idNo.slice(6, 10)).toString()
                 cb()
               }
               // this.loading = false
@@ -89,7 +89,6 @@
         }
       }
       const validateAge = (rule, age, cb) => {
-        console.log(age);
         if(!age){
           cb(new Error('请输入年龄'))
         }else{
@@ -190,8 +189,7 @@
           email: [
             { type: 'email', message: '请输入正确的邮箱', trigger: 'blur' }
           ]
-        },
-        _age: null
+        }
       }
     },
     methods: {
@@ -228,7 +226,7 @@
         window.open('http://www.zilongshu.com/manager/course-adviser/export' + params)
       },
       openDetail({ code, realName, mobilePhone, idNo, sex, age, rate, email, id}) {
-        console.log('code, realName, mobilePhone, idNo, sex, age, rate, email, id -------> ', code, realName, mobilePhone, idNo, sex, age, rate, email, id);
+        // console.log('code, realName, mobilePhone, idNo, sex, age, rate, email, id -------> ', code, realName, mobilePhone, idNo, sex, age, rate, email, id);
 
         this.detailModal = true
         this.$refs['updateDetailRef'].resetFields()
@@ -237,7 +235,8 @@
       updateDetail() {
         this.$refs['updateDetailRef'].validate(valid => {
           if(valid){
-            this._age && (this.updateDetailData.age = this._age)
+            this.loading = true
+            // this._age && (this.updateDetailData.age = this._age)
             http.post({
               vm: this,
               url: '/manager/course-adviser/edit',
@@ -248,6 +247,10 @@
                 this.updateDetailData = {}
                 this.$refs['updateDetailRef'].resetFields()
                 this.getCourseAdviserList()
+                this.loading = false
+              },
+              error: err => {
+                this.loading = false
               }
             })
           }
